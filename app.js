04f3ad3e25cc09e6039e6f8c588034c3297f6e58
Shahwaiz Nasir <154569201+shahwaiz0605@users.noreply.github.com>
2024-01-22 20:02:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+alert("Hello World")
+confirm("Do You Want To Processd?")
+var inp = prompt("Enter Your Number")
+
+document.write(`${inp} x 1 =`, inp*1 + "<br>")
+document.write(`${inp} x 2 =`, inp*2 + "<br>")
+document.write(`${inp} x 3 =`, inp*3 + "<br>")
+document.write(`${inp} x 4 =`, inp*4 + "<br>")
+document.write(`${inp} x 5 =`, inp*5 + "<br>")
+document.write(`${inp} x 6 =`, inp*6 + "<br>")
+document.write(`${inp} x 7 =`, inp*7 + "<br>")
+document.write(`${inp} x 8 =`, inp*8 + "<br>")
+document.write(`${inp} x 9 =`, inp*9 + "<br>")
+document.write(`${inp} x 10 =`, inp*10 + "<br>")
+
